7eeb8d1daab7df427092b4a705d4189e2df17e54
igorchirkin <igor@webstuff.co.il>
2022-04-10 11:30:59 +0300

    resolved issues

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -10,6 +10,7 @@ import { IssueReportComponent } from '../issue-report/issue-report.component';
 })
 export class IssueListComponent implements OnInit {
   showReportIssue = false;
+  selectedIssue: Issue | null = null;
   issues: Issue[] = [];
   constructor(private issueService: IssuesService) {}
 
@@ -23,5 +24,13 @@ export class IssueListComponent implements OnInit {
   onCloseReport() {
     this.showReportIssue = false;
     this.getIssues();
+  }
+
+  onConfirm(confirmed: boolean){
+    if(confirmed && this.selectedIssue){
+      this.issueService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
   }
 }
--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Issue } from './issue';
-import {issues} from '../assets/mocjkup-issues'
+import { issues } from '../assets/mocjkup-issues';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IssuesService {
   private issues: Issue[] = issues;
-  constructor() { }
+  constructor() {}
 
   getPendingIssues(): Issue[] {
-    return this.issues.filter(issue =>
-    !issue.completed);
-    }
+    return this.issues.filter((issue) => !issue.completed);
+  }
 
-    createIssue(issue: Issue) {
-      issue.issueNo = this.issues.length + 1;
-      this.issues.push(issue);
-     }
+  createIssue(issue: Issue) {
+    issue.issueNo = this.issues.length + 1;
+    this.issues.push(issue);
+  }
+
+  completeIssue(issue:Issue){
+    const selectedIssue: Issue = {
+      ...issue, completed: new Date()
+    };
+    const index = this.issues.findIndex(i => i===issue);
+    this.issues[index] = selectedIssue;
+  }
 }
